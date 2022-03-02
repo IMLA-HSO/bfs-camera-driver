1b5782c10a4709145a77045ebbb454c108767291
sandesh-1997 <92570618+sandesh-1997@users.noreply.github.com>
2022-03-02 22:49:34 +0100

    Delete copy-Flo-Files-Both.py

--- CSV Files/copy-Flo-Files-Both.py
+++
@@ -1,9 +0,0 @@
-import glob
-import shutil
-import os
-
-src_dir = '../flownet2-docker/flownet-Car/data'
-dst_dir = '../flownet2-docker/flownet-Car/data/car_both_data_flow'
-
-for flofile in glob.iglob(os.path.join(src_dir, '*.flo')):
-    shutil.move(flofile, dst_dir)
